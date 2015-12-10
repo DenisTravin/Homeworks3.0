348087389d3b689972d12d1b68c98b7be7e0e565
Denis <den_vesok@mail.ru>
2015-12-10 22:29:57 +0300

    Now I issue it nice and it's ready to check

---
+++ cycleList/cycleList/functions.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+struct ListElement
+{
+	int number;
+	ListElement *next;
+};
+
+struct ListHead
+{
+	ListElement *head;
+};
+
+int seacrhLastWarrior(ListHead* list, int num, int mum)
+{
+	ListElement* temp = list->head;
+	while (num > 1)
+	{
+		//printList(list, n - 1); //if you want to check program work - uncomment
+		for (int i = 0; i < mum - 1; i++)
+		{
+			temp = temp->next;
+		}
+		if (list->head == temp->next)
+		{
+			list->head = temp->next->next;
+		}
+		delElement(temp);
+		num--;
+	}
+	int result = temp->number;
+	delete temp;
+	delete list;
+	return result;
+}
+
+ListHead* makeListHead(int num)
+{
+	ListHead* list = new ListHead;
+	ListElement *newElement = new ListElement;
+	ListElement *temp = newElement;
+	list->head = newElement;
+	newElement->next = nullptr;
+	newElement->number = 0;
+	for (int i = 1; i < num; i++)
+	{
+		ListElement *newElement = new ListElement;
+		temp->next = newElement;
+		temp = newElement;
+		newElement->number = i;
+	}
+	temp->next = list->head;
+	temp = list->head;
+	temp->number = num;
+	return list;
+}
+
+ListElement* addElement(ListElement** source, ListElement* next,  int number)
+{
+	ListElement *newElement = new ListElement;
+	(*source)->next = newElement;
+	(*source) = newElement;
+	newElement->number = number;
+	newElement->next = next;
+	return newElement;
+}
+
+void printList(ListHead *list, int num)//print out all list
+{
+	if (list->head == nullptr)
+	{
+		cout << "List is empty!" << endl;
+		return;
+	}
+	ListElement *temp = list->head->next;
+	for (int i = 0; i < num; i++)
+	{
+		cout << temp->number << " ";
+		temp = temp->next;
+	}
+	cout << temp->number << " " << endl;
+}
+
+void delElement(ListElement *element)
+{
+	ListHead keapNextElement;
+	keapNextElement.head = element->next->next;
+	delete element->next;
+	element->next = keapNextElement.head;
+}
