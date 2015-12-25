8f8f9ee90d9494f43e727d144299f3599f2b8fde
Denis <den_vesok@mail.ru>
2015-12-25 18:09:10 +0300

    now it's work.

--- citiesAndRoads/citiesAndRoads/main.cpp
+++ citiesAndRoads/citiesAndRoads/main.cpp
@@ -1,126 +1,156 @@
 #include <iostream>
-#include <cstdio>
+#include <fstream>
 
-const int maxCity = 100;//max of cities 
+using namespace std;
 
-int min(int a, int b)//return minimum between 2 int numbers
+const int maxNumber = 100;
+
+int** setMatrix(int vertex)
 {
-	if (a > b)
+	int **matrix = new int*[vertex];
+	for (int i = 0; i < vertex; i++)
 	{
-		return b;
+		matrix[i] = new int[vertex];
 	}
-	else
+	for (int i = 0; i < vertex; i++)
 	{
-		return a;
+		for (int j = 0; j < vertex; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+	return matrix;
+}
+
+void deleteMatrix(int* matrix[], int vertex)
+{
+	for (int i = 0; i < vertex; i++)
+	{
+		delete[]matrix[i];
+	}
+	delete[]matrix;
+}
+
+int getNearVertex(int* matrix[], int vertex, bool used[], int start, int &minLength)
+{
+	int vertexTemp = -1;
+	for (int i = 0; i < vertex; i++)
+	{
+		if (matrix[i][start] > 0 && !used[i])
+		{
+			if (matrix[i][i] == matrix[start][start])
+			{
+				used[i] = true;
+				int newVertex = getNearVertex(matrix, vertex, used, i, minLength);
+				if (newVertex != -1)
+				{
+					vertexTemp = newVertex;
+				}
+			}
+			else
+			{
+				if (matrix[i][i] == -1)
+				{
+					int newLength = matrix[start][i];
+					if (newLength < minLength)
+					{
+						minLength = newLength;
+						vertexTemp = i;
+					}
+				}
+			}
+		}
+	}
+	return vertex;
+}
+
+int getNearCity(int* matrix[], int vertex, int currCountry)
+{
+	bool used[maxNumber] = {};
+	for (int i = 0; i < vertex; i++)
+	{
+		if (matrix[i][i] == currCountry)
+		{
+			used[i] = true;
+			int minLength = INT_MAX;
+			return getNearVertex(matrix, vertex, used, i, minLength);
+		}
 	}
 }
 
-void main(void)
+void setCities(int* matrix[], int vertex, int k)
 {
-	int matrixGraph[maxCity][maxCity] = {};
-	int m = 0;
-	int n = 0;
-	FILE *in = fopen("in.txt", "r");
-	fscanf(in, "%i %i", &n, &m);
-	//fill graph matrix by max numbers
-	for (int i = 0; i < n; i++)
+	int currCountry = 0;
+	int count = 0;
+	while (count < vertex - k)
 	{
-		for (int j = 0; j < n; j++)
+		int newCity = getNearCity(matrix, vertex, currCountry);
+		if (newCity == -1)
 		{
-			if (i != j)
-			{
-				matrixGraph[i][j] = INT16_MAX;
-			}
+			currCountry++;
+			currCountry = currCountry % k;
+			continue;
 		}
+		count++;
+		matrix[newCity - 1][newCity - 1] = currCountry + 1;
+		currCountry++;
+		currCountry = currCountry % k;
 	}
-	//add edges to graph matrix
-	for (int i = 0; i < m; i++)
-	{
-		int a = 0;
-		int b = 0;
-		int len = 0;
-		fscanf(in, "%i %i %i", &a, &b, &len);
-		matrixGraph[a - 1][b - 1] = len;
-		matrixGraph[b - 1][a - 1] = len;
-	}
-	int capitals[maxCity];//array of capitals 
-	int k = 0;
-	fscanf(in, "%i", &k);
-	for (int i = 0; i < k; i++)
-	{
-		fscanf(in, "%i", &capitals[i]);
-		capitals[i]--;
-	}
-	fclose(in);
-	//Floyd algorithm 
-	for (int i = 0; i < n; i++)
-	{
-		for (int u = 0; u < n; u++)
-		{
-			for (int v = 0; v < n; v++)
-			{
-				matrixGraph[u][v] = min(matrixGraph[u][v], matrixGraph[u][i] + matrixGraph[i][v]);
-			}
-		}
-	}
+}
 
-	int cities[maxCity];//array for capitals for all cities
-						//finding capital for each city
-	for (int i = 0; i < n; i++)
+void printCountry(int* matrix[], int vertex, int currCountry)
+{
+	bool used[maxNumber] = {};
+	for (int i = 0; i < vertex; i++)
 	{
-		cities[i] = capitals[0];
-		for (int j = 1; j < k; j++)
+		if (!used[i] && matrix[i][i] == currCountry)
 		{
-			if (matrixGraph[i][cities[i]] >= matrixGraph[i][capitals[j]])
-			{
-				cities[i] = capitals[j];
-			}
+			used[i] = true;
+			cout << i + 1 << ' ';
 		}
 	}
-	//output:
+}
+
+void main()
+{
+	int n = 0;
+	int m = 0;
+	ifstream file("input.txt", ios::in);
+	if (!file.is_open())
+	{
+		cout << "File don't found" << endl;
+		scanf("%*s");
+		return;
+	}
+	file >> n >> m;
+	int** matrix = setMatrix(n);
+	for (int k = 0; k < m; k++)
+	{
+		int i = 0;
+		int j = 0;
+		int len = 0;
+		file >> i;
+		file >> j;
+		file >> len;
+		matrix[i - 1][j - 1] = len;
+		matrix[j - 1][i - 1] = len;
+	}
+	int k = 0;
+	file >> k;
 	for (int i = 0; i < k; i++)
 	{
-		printf("\n%i country: ", i + 1);
-		for (int j = 0; j < n; j++)
-		{
-			if (cities[j] == capitals[i])
-			{
-				printf("%i ", j + 1);
-			}
-		}
+		int vertex = 0;
+		file >> vertex;
+		matrix[vertex - 1][vertex - 1] = i + 1;
 	}
+	file.close();
+	setCities(matrix, n, k);
+	for (int i = 0; i < k; i++)
+	{
+		cout << i + 1 << " country" << ": ";
+		printCountry(matrix, n, i);
+		cout << endl;
+	}
+	deleteMatrix(matrix, n);
 	scanf("%*s");
 }
-/*
-Test 1:
-Input:
-7 8
-5 1 3
-5 2 2
-5 3 2
-5 4 1
-5 6 2
-7 1 1
-7 4 2
-7 6 1
-2
-5 6
-Output:
-1 country: 2 3 4 5
-2 country: 1 6 7
-Test 2:
-Input:
-8 7
-2 3 1
-2 4 1
-2 5 1
-2 1 5
-1 6 1
-1 7 1
-1 8 1
-2
-1 2
-Output:
-1 country: 1 6 7 8
-2 country: 2 3 4 5
-*/
