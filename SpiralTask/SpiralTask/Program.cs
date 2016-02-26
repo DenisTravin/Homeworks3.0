c18e54591e7db3ed8ee692b31fbb84fe7e2e59aa
Denis <den_vesok@mail.ru>
2016-02-26 14:12:16 +0300

    Spiral task is ready.

--- SpiralTask/SpiralTask/Program.cs
+++ SpiralTask/SpiralTask/Program.cs
@@ -12,6 +12,8 @@ namespace SortArrayProgram
             int[,] array = new int[arrayNumber, arrayNumber] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 8 }, { 1, 2, 3, 4, 5 }, { 9, 8, 7, 6, 5 }, { 5, 4, 3, 2, 1 } };
             Console.Write("Input array: ");
             ArrayOutput(array, arrayNumber);
+            Console.Write("\nOutput spiral element: ");
+            ArraySpiralOutput(array, arrayNumber);
             Console.ReadLine();
         }
 
@@ -31,9 +33,40 @@ namespace SortArrayProgram
         // array spiral output function
         private static void ArraySpiralOutput(int[,] array, int arrayNumber)
         {
-            int iCurrentElement = arrayNumber / 2 + 1;
-            int jCurrentElement = arrayNumber / 2 + 1;
-            for (var i = iCurrentElement; i )
+            int iCurrentElement = arrayNumber / 2;
+            int jCurrentElement = arrayNumber / 2;
+            int step = 0;
+            Console.Write("{0} ", array[iCurrentElement, jCurrentElement]);
+            for (var k = 0; k < arrayNumber / 2; k++)
+            {
+                step++;
+                for (var i = 0; i < step; i++)
+                {
+                    iCurrentElement--;
+                    Console.Write("{0} ", array[iCurrentElement, jCurrentElement]);
+                }
+                for (var j = 0; j < step; j++)
+                {
+                    jCurrentElement++;
+                    Console.Write("{0} ", array[iCurrentElement, jCurrentElement]);
+                }
+                step++;
+                for (var i = 0; i < step; i++)
+                {
+                    iCurrentElement++;
+                    Console.Write("{0} ", array[iCurrentElement, jCurrentElement]);
+                }
+                for (var j = 0; j < step; j++)
+                {
+                    jCurrentElement--;
+                    Console.Write("{0} ", array[iCurrentElement, jCurrentElement]);
+                }
+            }
+            for (var i = 0; i < step; i++)
+            {
+                iCurrentElement--;
+                Console.Write("{0} ", array[iCurrentElement, jCurrentElement]);
+            }
         }
     }
 }
