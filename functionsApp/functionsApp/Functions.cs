d2998c461fc86598cca39b58bd8a13d84091d0a1
Denis Travin <den_vesok@mail.ru>
2016-05-08 23:18:59 +0300

    All 3 function is ready.

---
+++ functionsApp/functionsApp/Functions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ListRealisation;
+
+namespace Functions
+{
+    public class FunctionsClass
+    {
+        /// <summary>
+        /// map function realization
+        /// </summary>
+        /// <param name="list">input list</param>
+        /// <param name="function">input function</param>
+        /// <returns>map transformed list</returns>
+        public static List Map(List list, Func<int, int> function)
+        {
+            List mapList = new List();
+            for (var i = 0; i < list.GetListLenght(); i++)
+            {
+                mapList.AddElement(function(list.GetElement(i)));
+            }
+            return mapList;
+        }
+
+        /// <summary>
+        /// filter function realization
+        /// </summary>
+        /// <param name="list">input list</param>
+        /// <param name="function">input function</param>
+        /// <returns>filter transformed list</returns>
+        public static List Filter(List list, Func<int, bool> function)
+        {
+            List filterList = new List();
+            for (var i = 0; i < list.GetListLenght(); i++)
+            {
+                if (function(list.GetElement(i)))
+                {
+                    filterList.AddElement(list.GetElement(i));
+                }
+            }
+            return filterList;
+        }
+
+        /// <summary>
+        /// fold function realization
+        /// </summary>
+        /// <param name="list">input list</param>
+        /// <param name="initValue">initialization number value</param>
+        /// <param name="function">input function</param>
+        /// <returns>next accanuleted value</returns>
+        public static int Fold(List list, int initValue, Func<int, int, int> function)
+        {
+            for (var i = 0; i < list.GetListLenght(); i++)
+            {
+                initValue = function(initValue, list.GetElement(i));
+            }
+            return initValue;
+        }
+    }
+
+
+}
---
+++ functionsApp/functionsApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ListRealisation;
+using Functions;
+
+namespace FunctionsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List();
+            for (var i = 1; i < 4; i++)
+            {
+                list.AddElement(i);
+            }
+            list.ListOutput();
+
+            List mapList = FunctionsClass.Map(list, x => x * 2);
+            mapList.ListOutput();
+
+            List filterList = FunctionsClass.Filter(list, x => x % 2 == 1);
+            filterList.ListOutput();
+
+            Console.WriteLine("{0}", FunctionsClass.Fold(list, 1, (acc, elem) => acc * elem));
+            Console.ReadLine();
+        }
+    }
+}
