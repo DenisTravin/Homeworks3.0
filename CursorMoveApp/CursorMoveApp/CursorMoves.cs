9bb998586d2252afe4e397bc423e2e527fde50fc
Denis Travin <den_vesok@mail.ru>
2016-05-21 18:22:47 +0300

    Add cursor moves app.

---
+++ CursorMoveApp/CursorMoveApp/CursorMoves.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CursorMoveApp
+{
+    /// <summary>
+    /// class for making cursor moves
+    /// </summary>
+    public class CursorMoves
+    {
+        /// <summary>
+        /// cursor left move
+        /// </summary>
+        public void OnLeft(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft > 0)
+            {
+                Console.CursorLeft -= 1;
+            }
+        }
+
+        /// <summary>
+        /// cursor right move
+        /// </summary>
+        public void OnRight(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft < Console.WindowWidth - 1)
+            {
+                Console.CursorLeft += 1;
+            }
+        }
+
+        /// <summary>
+        /// cursor up move
+        /// </summary>
+        public void OnUp(object sender, EventArgs args)
+        {
+            if (Console.CursorTop > 0)
+            {
+                Console.CursorTop -= 1;
+            }
+        }
+
+        /// <summary>
+        /// cursor down move
+        /// </summary>
+        public void OnDown(object sender, EventArgs args)
+        {
+            if (Console.CursorTop < Console.WindowHeight - 1)
+            {
+                Console.CursorTop += 1;
+            }
+        }
+    }
+}
---
+++ CursorMoveApp/CursorMoveApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CursorMoveApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            var cursorManager = new CursorMoves();
+            eventLoop.RightHandler += cursorManager.OnRight;
+            eventLoop.LeftHandler += cursorManager.OnLeft;
+            eventLoop.UpHandler += cursorManager.OnUp;
+            eventLoop.DownHandler += cursorManager.OnDown;
+            eventLoop.Run();
+        }
+    }
+}
