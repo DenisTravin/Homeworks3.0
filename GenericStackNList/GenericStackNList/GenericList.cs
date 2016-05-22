a9f5117c62505c2100a41f931c63e305b0d68384
Denis Travin <den_vesok@mail.ru>
2016-05-22 22:17:35 +0300

    Generic list with list enumerator is ready for check.

--- GenericStackNList/GenericStackNList/GenericList.cs
+++ GenericStackNList/GenericStackNList/GenericList.cs
@@ -1,25 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericStackNList
 {
-    public class GenericList<T>
+    public class GenericList<T> : IEnumerable<T>
     {
         /// <summary>
         /// element class
         /// </summary>
         private class Element
         {
-            public int value;
+            public T value;
+
             public Element next;
 
-            public Element()
+            public Element(T newValue)
             {
-                this.value = 0;
-                this.next = null;
+                value = newValue;
             }
         }
 
@@ -27,21 +25,21 @@ namespace GenericStackNList
         /// list head
         /// </summary>
         private Element listHead;
+
         /// <summary>
         /// size of list
         /// </summary>
         private int listSize;
 
-        public List()
-        {
-            this.listHead = null;
-            this.listSize = 0;
-        }
 
-        public void AddElement(int newElementNumber)
+        /// <summary>
+        /// add element to list
+        /// </summary>
+        /// <param name="newElementValue"> value of new element</param>
+        public void AddElement(T newElementValue)
         {
-            Element newElement = new Element();
-            newElement.value = newElementNumber;
+            Element newElement = new Element(newElementValue);
+
 
             if (listSize == 0)
             {
@@ -59,13 +57,18 @@ namespace GenericStackNList
             listSize++;
         }
 
-        public bool DeleteElement(int elementNumber)
+        /// <summary>
+        /// delete element form list
+        /// </summary>
+        /// <param name="elementNumber">value of element to delete</param>
+        /// <returns>Does elemment delete?</returns>
+        public bool DeleteElement(T elementNumber)
         {
             if (listHead == null)
             {
                 return false;
             }
-            if (listHead.value == elementNumber)
+            if (listHead.value.Equals(elementNumber))
             {
                 listHead = listHead.next;
                 listSize--;
@@ -75,7 +78,7 @@ namespace GenericStackNList
             Element nextTempElement = listHead.next;
             while (tempElement.next != null)
             {
-                if (nextTempElement.value == elementNumber)
+                if (nextTempElement.value.Equals(elementNumber))
                 {
                     tempElement.next = nextTempElement.next;
                     listSize--;
@@ -87,16 +90,35 @@ namespace GenericStackNList
             return false;
         }
 
+        /// <summary>
+        /// return list lenght
+        /// </summary>
+        /// <returns>lenght of list</returns>
         public int GetListLenght()
         {
             return listSize;
         }
 
-        public int GetElement(int elementIndex)
+        /// <summary>
+        /// check list emptiness
+        /// </summary>
+        /// <returns>if list empty - return true, other - false</returns>
+        public bool IsEmpty()
+        {
+            return listSize == 0;
+        }
+
+        /// <summary>
+        /// return element by index
+        /// </summary>
+        /// <param name="elementIndex">element index</param>
+        /// <returns>element with input index(-1 if list don't 
+        /// include element with input index</returns>
+        public T GetElement(int elementIndex)
         {
             if (elementIndex >= listSize)
             {
-                return -1;
+                throw new LackElementException();
             }
             Element tempElement = listHead;
             for (var i = 0; i < elementIndex; i++)
@@ -106,12 +128,18 @@ namespace GenericStackNList
             return tempElement.value;
         }
 
-        public int GetElementIndex(int elementNumber)
+        /// <summary>
+        /// returnn index of element with input number
+        /// </summary>
+        /// <param name="elementNumber">element number</param>
+        /// <returns>index of element(-1 if list don't have element 
+        ///  with input index</returns>
+        public int GetElementIndex(T elementValue)
         {
             Element tempElement = listHead;
             for (var i = 0; i < listSize; i++)
             {
-                if (tempElement.value == elementNumber)
+                if (tempElement.value.Equals(elementValue))
                 {
                     return i;
                 }
@@ -120,6 +148,9 @@ namespace GenericStackNList
             return -1;
         }
 
+        /// <summary>
+        /// output list
+        /// </summary>
         public void ListOutput()
         {
             if (listHead == null)
@@ -134,6 +165,116 @@ namespace GenericStackNList
             }
             Console.WriteLine();
         }
-    }
+
+        /// <summary>
+        /// Implementation for the GetEnumerator method.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumeratorReal();
+        }
+
+        public ListEnumerator GetEnumeratorReal()
+        {
+            return new ListEnumerator(this);
+        }
+
+        /// <summary>
+        /// Implementation for the ListEnumerator method.
+        /// </summary>
+        public class ListEnumerator : IEnumerator<T>
+        {
+
+            int pos = -1;
+
+            GenericList<T> list;
+
+            Element curElement;
+
+            /// <summary>
+            /// ListEnumerator constructor
+            /// </summary>
+            /// <param name="list"></param>
+            public ListEnumerator(GenericList<T> list)
+            {
+                this.list = list;
+                curElement = null;
+            }
+
+            /// <summary>
+            /// Take next list element
+            /// </summary>
+            /// <returns>Does element contains in list</returns>
+            public bool MoveNext()
+            {
+                if (curElement != null)
+                {
+                    curElement = curElement.next;
+                }
+                else
+                {
+                    curElement = list.listHead;
+                }
+                pos++;
+                return pos < list.GetListLenght();
+            }
+
+            /// <summary>
+            /// Reset ListEnumerator
+            /// </summary>
+            public void Reset()
+            {
+                pos = -1;
+                curElement = null;                
+            }
+
+            /// <summary>
+            /// Get current list element
+            /// </summary>
+            public T Current
+            {
+                get
+                {
+                    return curElement.value;
+                }
+            }
+
+            /// <summary>
+            /// Get current list element in enumerator
+            /// </summary>
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return curElement.value;
+                }
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new ListEnumerator(this);
+        }
+
+        /// <summary>
+        /// Exeption call's if element don't standing in list
+        /// </summary>
+        [Serializable]
+        public class LackElementException : ApplicationException
+        {
+            public LackElementException() { }
+            public LackElementException(string message) : base(message) { }
+            public LackElementException(string message, Exception inner) : base(message, inner) { }
+            protected LackElementException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context)
+            { }
+        }
+    }   
 }
-}
+
--- GenericStackNList/GenericStackNList/Program.cs
+++ GenericStackNList/GenericStackNList/Program.cs
@@ -6,6 +6,7 @@ namespace GenericStackNList
     {
         static void Main(string[] args)
         {
+            //Stack check
             const int numberStackElements = 5;
             var stack = new GenericStack<string>();
             Console.WriteLine("Stack is empty? - {0}", stack.IsEmpty());
@@ -18,6 +19,28 @@ namespace GenericStackNList
                 Console.Write("{0} ", stack.Pop());
             }
             Console.WriteLine("\nStack is empty? - {0}", stack.IsEmpty());
+            //Generic list check
+            var list = new GenericList<string>();
+            for (var i = 1; i <= numberStackElements; i++)
+            {
+                list.AddElement((i * i).ToString());
+            }
+            list.ListOutput();
+            list.DeleteElement("25");
+            list.ListOutput();
+            Console.WriteLine("{0}", list.GetElementIndex("4"));
+
+            ///ListEnumerator check
+            var listEnum = new GenericList<int>();
+            for (var i = 0; i < 5; i++)
+            {
+                listEnum.AddElement(i);
+            }
+            foreach (var value in listEnum)
+            {
+                Console.WriteLine("{0}", value);
+            }
+
             Console.ReadLine();
         }
     }
