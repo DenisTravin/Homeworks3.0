e3f937a36f0b1f0d9fdb8d0d4fdcd26f562c65c6
Denis <den_vesok@mail.ru>
2016-05-27 01:22:38 +0300

    System commit

---
+++ genericBubble/genericBubble/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBubble
+{
+    class BubbleSort
+    {
+        static public List<T> Sort<T>(List<T> list, Func<T, T, bool> CompareFunc)
+        {
+            for (var i = 0; i < list.Count(); i++)
+            {
+                for (var j = 0; j < list.Count() - 1; j++)
+                {
+                    if (CompareFunc(list[j], list[j + 1]))
+                    {
+                        T highValue = list[j];
+
+                        list[j] = list[j + 1];
+                        list[j + 1] = highValue;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+    class Program
+    {
+
+        public static bool CompareObj(string x, string y)
+        {
+            return x.Length > y.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("pi");
+            list.Add("pop");
+            list.Add("p");
+            list.Add("pipop");
+            list.Add("pipo");
+
+            List<string> sortedList = BubbleSort.Sort(list, CompareObj);
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}", list[i]);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ genericBubble/genericBubble/SortRealisation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBubbleRealisation
+{
+    public class SortRealisation<T>
+    {
+        public List<T> BubbleSort(List<T> inputList, Func<T, T, bool> compareFunc)
+        {
+            for (var i = 0; i < inputList.Count(); i++)
+            {
+                for (var j = 0; j < inputList.Count(); j++)
+                {
+                    if (compareFunc(inputList[j], inputList[j+1]))
+                    {
+                        T highValue = inputList[j];
+
+                        inputList[j] = inputList[j + 1];
+                        inputList[j + 1] = highValue;
+                    }
+                }
+            }
+            return inputList;
+        }
+    }
+}
