6f007a088b5ea3acc02eb82e0a015674dce58a68
Denis <den_vesok@mail.ru>
2016-05-27 02:49:16 +0300

    Stack calculator is ready.

---
+++ StackCalc/StackCalc/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackInterfaceClass;
+
+namespace CalculatorNamespace
+{
+    /// <summary>
+    /// calculator realisation class
+    /// </summary>
+    public class Calculator
+    {
+        private static bool IsNumber(char inputChar)
+        {
+            return inputChar >= '0' && inputChar <= '9';
+        }
+
+        private static bool IsOperation(char inputChar)
+        {
+            return inputChar == '+' || inputChar == '-' || inputChar == '*' || inputChar == '/';
+        }
+
+        public static int CalculatorMethod(StackInterface inputStack, string inputString)
+        {
+            for (var i = 0; i < inputString.Length; i++)
+            {
+                if (IsNumber(inputString[i]))
+                {
+                    inputStack.Push(inputString[i] - '0');
+                    continue;
+                }
+
+                if (IsOperation(inputString[i]))
+                {
+                    int firstTemp;
+                    int secondTemp;
+
+                    int charTemp = inputString[i];
+
+                    if (inputStack.IsEmpty())
+                    {
+                        throw new LackElementException();
+                    }
+                    else
+                    {
+                        secondTemp = inputStack.Pop();
+                    }
+
+                    if (inputStack.IsEmpty())
+                    {
+                        throw new LackElementException();
+                    }
+                    else
+                    {
+                        firstTemp = inputStack.Pop();
+                    }
+
+                    switch (charTemp)
+                    {
+                        case '+':
+                            inputStack.Push(firstTemp + secondTemp);
+                            break;
+                        case '-':
+                            inputStack.Push(firstTemp - secondTemp);
+                            break;
+                        case '*':
+                            inputStack.Push(firstTemp * secondTemp);
+                            break;
+                        case '/':
+                            inputStack.Push(firstTemp / secondTemp);
+                            break;
+                    }
+                }
+
+            }
+            return inputStack.Pop();
+        }
+
+        [Serializable]
+        public class LackElementException : ApplicationException
+        {
+            public LackElementException() { }
+            public LackElementException(string message) : base(message) { }
+            public LackElementException(string message, Exception inner) : base(message, inner) { }
+            protected LackElementException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context)
+            { }
+        }
+
+    }
+}
---
+++ StackCalc/StackCalc/StackListRealisation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StackInterfaceClass;
+
+namespace StackListRealisation
+{
+    /// <summary>
+    /// stack list realisation class
+    /// </summary>
+    public class StackList : StackInterface
+    {
+        /// <summary>
+        /// array of stack elements
+        /// </summary>
+        private List<int> stackList;
+
+        /// <summary>
+        /// stack constructor
+        /// </summary>
+        public StackList()
+        {
+            stackList = new List<int>();
+        }
+
+        public bool IsEmpty()
+        {
+            return stackList.Count() == 0;
+        }
+
+        public void Push(int number)
+        {
+            stackList.Add(number);
+        }
+
+        public int Pop()
+        {
+            int temp = stackList.Last();
+            stackList.RemoveAt(stackList.Count - 1);
+            return temp;
+        }
+    }
+}
